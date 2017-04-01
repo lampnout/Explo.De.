5cdc601f0c233a5e182a14fafa26e853eb6606ea
Lampros Noutsos <lampnout@users.noreply.github.com>
2017-04-01 14:15:37 +0300

    Create chargen.c

---
+++ CharGen/chargen.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+  printf("Payload size: 25 bytes\n");
+	printf("\"");
+	for (int i=0; i<256; i++){
+		if ((i%15)==0 && i!=0){
+		  printf("\"\n\"", i);
+		}
+		printf("\\x%02x", i);
+	}
+	printf("\"\n");
+	return 0;
+}
